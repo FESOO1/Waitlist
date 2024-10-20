d76aebe56172efd80f575eb84242b78b22c15f2f
FES001 <firdavs7794948@gmail.com>
2024-10-21 01:18:43 +0500

    Working on it.

--- script.js
+++ script.js
@@ -1,28 +1,27 @@
 const floatingObjects = document.querySelectorAll('.floating-object');
 let startPositionX = 0, startPositionY = 0, newPositionX = 0, newPositionY = 0;
 
+// DRAGGING FUNCTION
 
-for (let i = 0; i < floatingObjects.length; i++) {
-    floatingObjects[i].addEventListener('mousedown', e => {
-        startPositionX = e.clientX;
+/* for (let i = 0; i < floatingObjects.length; i++) {
+    floatingObjects[i].addEventListener('mousemove', e => {
         startPositionY = e.clientY;
+        startPositionX = e.clientX;
 
-
-        document.addEventListener('mousemove', moveFloatingObjects);
-
+        document.addEventListener('mousemove', mouseMoveFunction);
         document.addEventListener('mouseup', () => {
-            document.removeEventListener('mousemove', moveFloatingObjects);
+            document.removeEventListener('mousemove', mouseMoveFunction);
         });
+
     });
-
-    function moveFloatingObjects(e) {
-        newPositionX = startPositionX - e.clientX;
+    function mouseMoveFunction(e) {
         newPositionY = startPositionY - e.clientY;
+        newPositionX = startPositionX - e.clientX;
 
-        startPositionX = e.clientX;
         startPositionY = e.clientY;
+        startPositionX = e.clientX;
 
         floatingObjects[i].style.top = (floatingObjects[i].offsetTop - newPositionY) + 'px';
         floatingObjects[i].style.left = (floatingObjects[i].offsetLeft - newPositionX) + 'px';
     };
-};
+}; */
